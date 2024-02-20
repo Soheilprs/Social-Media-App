5b67dc9106e0252ad99498291ba753032264c9c3
1-Dex <1dex@blockto.ir>
2024-02-20 04:11:50 +0330

    Create base file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./globals.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+
+        {/* private routes */}
+        <Routes index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
